dd6770b29fe585576731d56644204b9304ba831f
KarlaNunes <julyananunes321@hotmail.com>
2020-12-20 18:32:07 -0300

    created header at form screen on branch telaCadastro

--- src/components/form/formulario.js
+++ src/components/form/formulario.js
@@ -5,7 +5,26 @@ export default props => {
     return (
         <body id="page-forms">
             <div id="container">
-                <h1>Hello World</h1>
+                <header className="page-header">
+                    <div className="top-bar-container">
+                        <a href="/">
+                            <img src="" alt=""/>
+                        </a>
+
+                        <img src="" alt="" />
+                    </div>
+
+                    <div className="header-content">
+                        <strong>Que incrível que você quer aprender</strong>
+                        <p>O primeiro passo, é preencher esse formulário de inscrição.</p>
+                    </div>
+
+
+                </header>
+
+                <main>
+
+                </main>
             </div>
         </body>
     )

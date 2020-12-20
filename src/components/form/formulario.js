b309fcadc2cb6f309316469aa14162e51542648f
KarlaNunes <julyananunes321@hotmail.com>
2020-12-20 17:28:24 -0300

    created form screen

---
+++ src/components/form/formulario.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default props => {
+
+    return (
+        <body id="page-forms">
+            <div id="container">
+                <h1>Hello World</h1>
+            </div>
+        </body>
+    )
+}

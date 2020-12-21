d493400e90380d57c655fd82edc1a74635eb56c5
KarlaNunes <julyananunes321@hotmail.com>
2020-12-21 14:17:53 -0300

    modified files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import LandingPage from './components/landing-page/landing-page';
-import Formulario from './components/form/formulario';
-
 
 function App() {
   return (
-    // <LandingPage/>,
-    <Formulario />
+    <LandingPage/>
   );
 }
 

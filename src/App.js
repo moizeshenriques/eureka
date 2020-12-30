15add0f1e652f2f516d6752d7e81716038f263db
KarlaNunes <julyananunes321@hotmail.com>
2020-12-30 18:29:12 -0300

    created page ' modulos'

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import LandingPage from './components/landing-page/landing-page';
 import Formulario from './components/form/formulario';
 import FutureYou from './components/future-you/future-you';
+import Modulos from './components/modulos/modulos';
 
 function App() {
   return (
     //<LandingPage/>
     // <Formulario />
-    <FutureYou />
+    // <FutureYou />
+    <Modulos />
   );
 }
 
---
+++ src/components/modulos/modulos.js
@@ -0,0 +1,80 @@
+import React from 'react';
+
+import '../form/form.css';
+import Header from '../header/header';
+
+export default props => {
+        return (
+            <body id="page-forms">
+                <div id="container">
+                    <Header />
+
+                    <main>
+                        <p><a href="">Vá para o nosso painel de inspiração</a></p>
+
+                        <div>
+                            <h3>O que é genética</h3>
+                            <p>Nesse módulo vamos retratar como surgiu a genética, seus conceitos fundamentais e figuras importantes nos primeiros estudos.</p>
+                        </div>
+
+                        <div>
+                            <h3>O que é genética</h3>
+                            <p>Nesse módulo vamos retratar como surgiu a genética, seus conceitos fundamentais e figuras importantes nos primeiros estudos.</p>
+                        </div>
+
+                        <div>
+                            <h3>O que é genética</h3>
+                            <p>Nesse módulo vamos retratar como surgiu a genética, seus conceitos fundamentais e figuras importantes nos primeiros estudos.</p>
+                        </div>
+
+                        <div>
+                            <h3>O que é genética</h3>
+                            <p>Nesse módulo vamos retratar como surgiu a genética, seus conceitos fundamentais e figuras importantes nos primeiros estudos.</p>
+                        </div>
+
+                        <div>
+                            <h3>O que é genética</h3>
+                            <p>Nesse módulo vamos retratar como surgiu a genética, seus conceitos fundamentais e figuras importantes nos primeiros estudos.</p>
+                        </div>
+
+                        <div>
+                            <h3>O que é genética</h3>
+                            <p>Nesse módulo vamos retratar como surgiu a genética, seus conceitos fundamentais e figuras importantes nos primeiros estudos.</p>
+                        </div>
+
+                        <div>
+                            <h3>O que é genética</h3>
+                            <p>Nesse módulo vamos retratar como surgiu a genética, seus conceitos fundamentais e figuras importantes nos primeiros estudos.</p>
+                        </div>
+
+                        <div>
+                            <h3>O que é genética</h3>
+                            <p>Nesse módulo vamos retratar como surgiu a genética, seus conceitos fundamentais e figuras importantes nos primeiros estudos.</p>
+                        </div>
+
+                        <div>
+                            <h3>O que é genética</h3>
+                            <p>Nesse módulo vamos retratar como surgiu a genética, seus conceitos fundamentais e figuras importantes nos primeiros estudos.</p>
+                        </div>
+
+                        <div>
+                            <h3>O que é genética</h3>
+                            <p>Nesse módulo vamos retratar como surgiu a genética, seus conceitos fundamentais e figuras importantes nos primeiros estudos.</p>
+                        </div>
+
+                        <div>
+                            <h3>O que é genética</h3>
+                            <p>Nesse módulo vamos retratar como surgiu a genética, seus conceitos fundamentais e figuras importantes nos primeiros estudos.</p>
+                        </div>
+
+                        <div>
+                            <h3>O que é genética</h3>
+                            <p>Nesse módulo vamos retratar como surgiu a genética, seus conceitos fundamentais e figuras importantes nos primeiros estudos.</p>
+                        </div>
+
+                        <button type="submit" form="create-class">Faça o nosso teste FUTURE YOU</button>
+                    </main>
+                </div>
+            </body>
+        );
+}
